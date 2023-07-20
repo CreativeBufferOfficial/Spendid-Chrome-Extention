f803699456dccaf2799e3a8f853cac7d5d9e622a
deepakcreativebuffer <deepak@creativebuffer.com>
2023-07-20 13:00:49 +0530

    Update BarChat

--- src/component/Charts/BarChart/BarChart.jsx
+++ src/component/Charts/BarChart/BarChart.jsx
@@ -21,101 +21,72 @@ const BarChart = ({ id }) => {
   const { loadingBudgets, budgets } = useSelector((state) => state.budget);
   const { lendings } = useSelector((state) => state.lending);
   const [savingsSet, setSavingsSet] = useState(false);
-  const [difference, setDifference] = useState([]);
-
-  // const init = () => {
-  //   const demographicsObjects = getStructureObject(demographics);
-  //   const budgetObjects = getStructureObject(budgets);
-  //   const demographicsCategory = filterCategory(demographicsObjects);
-  //   const budgetCategory = filterCategory(budgetObjects);
-  //   getTabData(demographicsCategory, budgetCategory);
-  //   // console.log(demographicsCategory);
-  //   const diff = getDifferenceToPeers(demographicsCategory);
-  //   console.log('difference', diff);
-  //   setDifference(diff);
-  // };
 
   useEffect(() => {
     // init();
-    const demographicsObjects = getStructureObject(demographics);
-    const budgetObjects = getStructureObject(budgets);
-    const demographicsCategory = filterCategory(demographicsObjects);
-    const budgetCategory = filterCategory(budgetObjects);
-    getTabData(demographicsCategory, budgetCategory);
-    // console.log(demographicsCategory);
-    const diff = getDifferenceToPeers(demographicsCategory);
-    console.log('difference', diff);
-
+    let diff;
+    if (demographics && budgets) {
+      const demographicsObjects = getStructureObject(demographics);
+      const budgetObjects = getStructureObject(budgets);
+      const demographicsCategory = filterCategory(demographicsObjects);
+      const budgetCategory = filterCategory(budgetObjects);
+      getTabData(demographicsCategory, budgetCategory);
+      // console.log(demographicsCategory);
+      diff = getDifferenceToPeers(demographicsCategory);
+      console.log('difference', diff);
+    }
     // Create chart instance
     const chart = am4core.create(id, am4charts.XYChart);
 
     // Add data
-    chart.data = diff.splice(0, 5);
-    // chart.data = [
-    //   { category: 'Category 1', difference: 10 },
-    //   { category: 'Category 2', difference: 20 },
-    //   { category: 'Category 3', difference: 15 },
-    //   { category: 'Category 4', difference: 12 },
-    //   { category: 'Category 5', difference: 10 },
-    //   { category: 'Category 6', difference: 12 },
-    //   // Add more data points as needed
-    // ];
+    chart.data = diff && diff.splice(0, 5);
 
     // Create axes
     const categoryAxis = chart.yAxes.push(new am4charts.CategoryAxis());
     categoryAxis.dataFields.category = 'category';
-    categoryAxis.renderer.inversed = true; // Invert the axis to make it horizontal
-    categoryAxis.title.text = '';
-    categoryAxis.renderer.grid.template.location = 0;
-    categoryAxis.renderer.labels.template.location = 0.5;
-    categoryAxis.renderer.labels.template.dy = -60; // Adjust the vertical position of the label
-    categoryAxis.renderer.labels.template.disabled = true; // Disable category labels
-    categoryAxis.renderer.minGridDistance = 1;
 
-    // Adjust the renderer to increase the distance between categories
-    categoryAxis.renderer.cellStartLocation = 0.4; // Change this value as needed
-    categoryAxis.renderer.cellEndLocation = 1; // Change this value as needed
+    categoryAxis.numberFormatter.numberFormat = '$#,###';
+    categoryAxis.renderer.inversed = true;
+    categoryAxis.renderer.grid.template.location = 0;
+    categoryAxis.renderer.cellStartLocation = 0.9;
+    categoryAxis.renderer.cellEndLocation = 0.7;
+    categoryAxis.renderer.labels.template.rotation = 0;
+    categoryAxis.renderer.labels.template.fill = am4core.color('#000');
+    categoryAxis.renderer.labels.template.verticalTop = 'top';
+    categoryAxis.renderer.labels.template.wrap = false;
+    categoryAxis.renderer.labels.template.truncate = false;
+    // categoryAxis.renderer.labels.template.maxWidth = 120;
+    categoryAxis.renderer.inside = true;
+    categoryAxis.renderer.labels.template.valign = 'top';
+    categoryAxis.renderer.labels.template.dy = -1;
+    categoryAxis.renderer.labels.template.dx = -10;
 
     const valueAxis = chart.xAxes.push(new am4charts.ValueAxis());
     valueAxis.title.text = 'Difference';
-    valueAxis.renderer.labels.template.fontSize = 12;
+    valueAxis.renderer.labels.template.fontSize = 10;
+    // valueAxis.renderer.labels.template.wrap = true;
 
     // Create series
     const series = chart.series.push(new am4charts.ColumnSeries());
     series.dataFields.valueX = 'difference';
     series.dataFields.categoryY = 'category';
     series.name = 'difference';
-    series.columns.template.tooltipText = '{name}: [bold]${valueX}[/]';
+    series.columns.template.tooltipText = '{name}: [bold]{valueX}[/]';
     // Set the color of the columns
     series.columns.template.fill = am4core.color('#0267e8');
 
     // Adjust column width to increase the distance between bars
     series.columns.template.columnWidth = 30; // Change this value as needed
 
-    // Add label bullet
-    const labelBullet1 = series.bullets.push(new am4charts.LabelBullet());
-    labelBullet1.label.text = '{categoryY}';
-    labelBullet1.label.dx = -5;
-    labelBullet1.label.dy = -20;
-    // Adjust the vertical position of the label
-    labelBullet1.label.fontSize = 12;
-    labelBullet1.label.horizontalCenter = 'right';
-    labelBullet1.label.verticalCenter = 'middle';
-
     const labelBullet2 = series.bullets.push(new am4charts.LabelBullet());
-    labelBullet2.label.text = '${valueX}';
-    labelBullet2.label.dx = 10; // Adjust the vertical position of the label
+    labelBullet2.label.text = '{valueX}';
+    labelBullet2.label.dx = 5; // Adjust the vertical position of the label
     labelBullet2.label.fontSize = 12;
     labelBullet2.label.horizontalCenter = 'left';
+    labelBullet2.label.fontSize = 13;
+    labelBullet2.label.hideOversized = false;
+    labelBullet2.label.truncate = false;
 
-    // Add value labels inside each bar
-    series.columns.template.adapter.add('text', (text, target) => {
-      const value = target.dataItem && target.dataItem.valueX;
-      if (difference && difference > 0) {
-        return (Math.round(difference / 1000) * 1000).toString(); // Change the value formatting as needed
-      }
-      return text;
-    });
     series.columns.template.adapter.add('textY', (textY, target) => {
       return -series.columns.template.height / 2;
     });
@@ -148,7 +119,7 @@ const BarChart = ({ id }) => {
     }
   }, [lendings, categoryInputHandler, savingsSet]);
 
-  return <div id={id} style={{ width: '100%', height: '350px' }}></div>;
+  return <div id={id} style={{ width: '100%', height: '340px' }}></div>;
 };
 
 export default BarChart;
